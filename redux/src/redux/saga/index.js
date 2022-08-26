36af07c14d48009ff08aa1b9658edfcb291c21e1
souravmahanty <souravmahanty12@gmail.com>
2022-08-26 17:03:12 +0530

    second commit

---
+++ redux/src/redux/saga/index.js
@@ -0,0 +1,11 @@
+// import { delay } from "redux-saga";
+import { takeLatest, put } from "redux-saga/effects";
+
+function* ageUpAsync() {
+//   yield delay(4000);
+  yield put({ type: "fetch_data", value: 1 });
+}
+
+ export function* watchAgeUp() {
+  yield takeLatest("fetch_data", ageUpAsync);
+}
